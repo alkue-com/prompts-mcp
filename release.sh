d1d4122a8f88cf28a0fb9f7c9e5e1d46b8d85ce0
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2025-09-19 19:46:09 +0300

    feat(release): add release script for automated version bumping and publishing

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+prerelease_type="$1"
+
+branch="$(git branch --show-current)"
+if [ "$branch" != "main" ] && [ "$branch" != "master" ]; then
+    echo "Error: Releases must be created from trunk, run $0 in main/master."
+    exit 1
+fi
+
+if [ -n "$(git status -s)" ]; then
+    echo "Error: Working tree has changes: Stash, commit or reset first"
+    exit 1
+fi
+
+if [ -n "$prerelease_type" ]; then
+    echo "Creating pre-release ($prerelease_type)"
+    uvx --from commitizen cz bump --prerelease "$prerelease_type" \
+        --allow-no-commit &&
+        pdm publish --repository testpypi
+else
+    uvx --from commitizen cz bump --allow-no-commit &&
+        pdm publish
+fi
